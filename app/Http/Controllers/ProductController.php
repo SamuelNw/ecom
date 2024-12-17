d10e143f8e9e0be2514190740ee871c0d23a16a8
Sam <samuelnganga.w@gmail.com>
2024-12-17 11:36:33 +0300

    Add a search route for the products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Search for products based on name or description.
+     */
+    public function search(Request $request)
+    {
+        // Get the search query from the request
+        $query = $request->input('query');
+
+        // Ensure the query is not empty
+        if (empty($query)) {
+            return response()->json([
+                'message' => 'No search term provided.'
+            ], 400);
+        }
+
+        // Perform the search using LIKE for matching name and description
+        $results = Product::where('name', 'LIKE', "%{$query}%")
+            ->orWhere('description', 'LIKE', "%{$query}%")
+            ->orderBy('name')
+            ->limit(20)
+            ->get();
+
+        // Return the results as JSON
+        return response()->json($results);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/search', [ProductController::class, 'search']);
 
 Route::get('/', function () {
     return view('welcome');
